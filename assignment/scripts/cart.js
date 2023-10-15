1fe5965c78a0175a1d8d8098574998d4fe4503df
Mark Klein <kleincentral@gmail.com>
2023-10-15 16:37:40 -0500

    Console logs added, functions added

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,12 +2,25 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
+
+function listItems(){
+    for(let i=0; i<basket.length; i++){
+        console.log('removing: ' + basket[i])
+    }
+
+}
 
 
-
-
-
-
+console.log('Basket list is now '+ basket);
+console.log('Adding apples, outcome should return true: ' + addItem('apples'));
+console.log('Basket is now ' + basket );
+console.log('Cleared basket list, list now should be undefined: ' + listItems())
 
 
 
